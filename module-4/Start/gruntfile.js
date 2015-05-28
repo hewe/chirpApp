15ae52482d5638674f011a492e311a3dcaaf43de
Ubuntu <weiping@devbox.defaultdomain>
2015-05-27 18:30:13 -0700

    Setup gruntfile.js to auto reload app

---
+++ module-4/Start/gruntfile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = function(grunt){
+	grunt.initConfig({
+		nodemon: {
+			all: {
+				script: 'bin/www',
+				options: {
+					watchedExtensions: ['js']
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-nodemon');
+
+	grunt.registerTask('default', ['nodemon']);
+};
